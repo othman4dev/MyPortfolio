3d144c67e22e8904f4655b25bc990dc254e88df6
othman4dev <othmandevsup@gmail.com>
2024-10-03 17:19:35 +0100

    finalizing the projects section.

--- script.js
+++ script.js
@@ -113,3 +113,29 @@ function searchTechs(input) {
         }
     }
 }
+
+const all = document.querySelectorAll(".card");
+
+window.addEventListener("mousemove", (ev) => {
+  
+  all.forEach((e) => {
+    const blob = e.querySelector(".blob");
+    const fblob = e.querySelector(".fakeblob");
+    const rec = fblob.getBoundingClientRect();
+    blob.style.opacity = "1";
+  
+    blob.animate(
+      [
+        {
+          transform: `translate(${
+            (ev.clientX - rec.left) - (rec.width / 2)
+          }px,${(ev.clientY - rec.top) - (rec.height / 2)}px)`
+        }
+      ],
+      {
+        duration: 300,
+        fill: "forwards"
+      }
+    );
+  });
+});
